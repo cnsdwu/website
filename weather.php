e550ed55149e9dd172fe5ac8787694520b948912
cnsdwu <cnsdwu@163.com>
2019-02-09 19:24:27 +0800

    Revert "first commit"
    This reverts commit 0ead0a5c0e47dc77ab7d7ac45b7e2d55a194fe85.

--- weather.php
+++
@@ -1,4 +0,0 @@
-<?php
-    header('Access-Control-Allow-Origin: *');
-    @$city = $_REQUEST['city']? $_REQUEST['city'] : '北京';
-    echo str_replace('http', 'https', file_get_contents('https://www.baidu.com/home/other/data/weatherInfo?city='.$city));
